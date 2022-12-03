568f0a1939356995a15def35a0632f63454fb0ee
Mark Jans <mark@markjans.nl>
2022-12-03 12:15:20 +0100

    Set to nightly chain - day 3: use array_chunks

--- day03/src/bin/part2.rs
+++ day03/src/bin/part2.rs
@@ -1,3 +1,5 @@
+#![feature(iter_array_chunks)]
+
 use day03::input;
 use std::collections::HashSet;
 
@@ -5,27 +7,22 @@ fn priority_sum(input: &str) -> usize {
     input
         .lines()
         .map(|line| line.bytes().collect::<HashSet<_>>())
-        .collect::<Vec<_>>()
-        .chunks(3)
-        .map(|rucksacks| {
-            if let [rucksack1, rucksack2, rucksack3] = rucksacks {
-                rucksack1
-                    .intersection(rucksack2)
-                    .copied()
-                    .collect::<HashSet<_>>()
-                    .intersection(rucksack3)
-                    .map(|&unique| {
-                        if unique >= 97 {
-                            unique as usize - 96
-                        } else {
-                            unique as usize - 38
-                        }
-                    })
-                    .next()
-                    .unwrap()
-            } else {
-                0
-            }
+        .array_chunks()
+        .map(|[rucksack1, rucksack2, rucksack3]| {
+            rucksack1
+                .intersection(&rucksack2)
+                .copied()
+                .collect::<HashSet<_>>()
+                .intersection(&rucksack3)
+                .map(|&unique| {
+                    if unique >= 97 {
+                        unique as usize - 96
+                    } else {
+                        unique as usize - 38
+                    }
+                })
+                .next()
+                .unwrap()
         })
         .sum()
 }
