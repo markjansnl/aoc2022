6ea1ddf587c7351d7b33bddaebde53e96433859b
Mark Jans <mark@markjans.nl>
2022-12-04 11:34:28 +0100

    Reordered test cases

--- utils/src/range_ext.rs
+++ utils/src/range_ext.rs
@@ -58,26 +58,26 @@ impl<Idx: PartialOrd<Idx>> RangeExt for RangeInclusive<Idx> {
 
 #[test]
 fn test_fully_contains() {
-    let lhs = 1..=6;
-    assert_eq!(true, lhs.fully_contains(&(2..=4)));
-    assert_eq!(false, lhs.fully_contains(&(4..=8)));
-    assert_eq!(false, lhs.fully_contains(&(8..=9)));
-
     let lhs = 1..6;
     assert_eq!(true, lhs.fully_contains(&(2..4)));
     assert_eq!(false, lhs.fully_contains(&(4..8)));
     assert_eq!(false, lhs.fully_contains(&(8..9)));
+
+    let lhs = 1..=6;
+    assert_eq!(true, lhs.fully_contains(&(2..=4)));
+    assert_eq!(false, lhs.fully_contains(&(4..=8)));
+    assert_eq!(false, lhs.fully_contains(&(8..=9)));
 }
 
 #[test]
 fn test_overlaps() {
-    let lhs = 1..=6;
-    assert_eq!(true, lhs.overlaps(&(2..=4)));
-    assert_eq!(true, lhs.overlaps(&(4..=8)));
-    assert_eq!(false, lhs.overlaps(&(8..=9)));
-
     let lhs = 1..6;
     assert_eq!(true, lhs.overlaps(&(2..4)));
     assert_eq!(true, lhs.overlaps(&(4..8)));
     assert_eq!(false, lhs.overlaps(&(8..9)));
+
+    let lhs = 1..=6;
+    assert_eq!(true, lhs.overlaps(&(2..=4)));
+    assert_eq!(true, lhs.overlaps(&(4..=8)));
+    assert_eq!(false, lhs.overlaps(&(8..=9)));
 }
