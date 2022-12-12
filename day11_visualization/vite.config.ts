2b668c45a8cdc162bc2e25f8c2155d306b3158ea
Mark Jans <mark@markjans.nl>
2022-12-12 07:43:42 +0100

    Updated base url

--- day11_visualization/vite.config.ts
+++ day11_visualization/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
-  base: '/aoc2022/day11_vizualization/dist/',
+  base: '/aoc2022/day11_visualization/dist/',
   plugins: [solidPlugin()],
   server: {
     port: 3000,
