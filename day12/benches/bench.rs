5d8e7cdc0eebd55933bcda105399211f022e9970
Mark Jans <mark@markjans.nl>
2022-12-12 11:55:28 +0100

    Day 12: cargo fmt

--- day12/benches/bench.rs
+++ day12/benches/bench.rs
@@ -1,17 +1,13 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use day12::{part1, part2, input};
+use day12::{input, part1, part2};
 
 pub fn criterion_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("day12");
     group.sample_size(20);
 
-    group.bench_function("part1", |b| {
-        b.iter(|| part1(black_box(input::USER)))
-    });
+    group.bench_function("part1", |b| b.iter(|| part1(black_box(input::USER))));
 
-    group.bench_function("part2", |b| {
-        b.iter(|| part2(black_box(input::USER)))
-    });
+    group.bench_function("part2", |b| b.iter(|| part2(black_box(input::USER))));
 
     group.finish();
 }
