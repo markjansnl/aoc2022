f1de22eef93005f14e5a77972c49c58f4b652307
Mark Jans <mark@markjans.nl>
2022-12-27 21:32:29 +0100

    Day 20: part 2

--- day20/src/bin/part1.rs
+++ day20/src/bin/part1.rs
@@ -1,76 +1,10 @@
-use std::collections::HashMap;
-
-use day20::input;
-
-fn grove_coordinates(input: &str) -> i16 {
-    let sequence: Vec<i16> = input.lines().map(|line| line.parse().unwrap()).collect();
-    let mut mixed = sequence.clone();
-
-    let mut map: HashMap<i16, (Vec<usize>, usize)> = HashMap::new();
-    for (index, number) in sequence.iter().copied().enumerate() {
-        map.entry(number)
-            .and_modify(|number_vec| number_vec.0.push(index))
-            .or_insert((vec![index], 0));
-    }
-
-    for number in sequence.iter() {
-        let index;
-        let next_index;
-
-        {
-            let map_item = map.get_mut(number).unwrap();
-            index = map_item.0[map_item.1];
-
-            let next_index_i16 = (index as i16 + number) % (sequence.len() as i16 - 1);
-            next_index = if next_index_i16 < 0 {
-                sequence.len() - 1 - (next_index_i16.abs() as usize % (sequence.len() - 1))
-            } else {
-                next_index_i16 as usize
-            };
-        }
-
-
-        assert!(index < sequence.len());
-        assert!(next_index < sequence.len());
-        if next_index > index {
-            for i in index + 1..=next_index {
-                map.entry(mixed[i]).and_modify(|(rotated_number_vec, _)| {
-                    for rotated_index in rotated_number_vec.iter_mut() {
-                        if *rotated_index == i {
-                            *rotated_index -= 1;
-                        }
-                    }
-                });
-            }
-            map.entry(*number).and_modify(|map_item| map_item.0[map_item.1] = next_index);
-            mixed[index..=next_index].rotate_left(1);
-        } else if index > next_index {
-            for i in next_index..index {
-                map.entry(mixed[i]).and_modify(|(rotated_number_vec, _)| {
-                    for rotated_index in rotated_number_vec.iter_mut() {
-                        if *rotated_index == i {
-                            *rotated_index += 1;
-                        }
-                    }
-                });
-            }
-            map.entry(*number).and_modify(|map_item| map_item.0[map_item.1] = next_index);
-            mixed[next_index..=index].rotate_right(1);
-        }
-        map.entry(*number).and_modify(|map_item| map_item.1 += 1);
-    }
-
-    let index0 = *map.get(&0).unwrap().0.first().unwrap();
-    mixed[(index0 + 1000) % sequence.len()]
-        + mixed[(index0 + 2000) % sequence.len()]
-        + mixed[(index0 + 3000) % sequence.len()]
-}
+use day20::{input, part1};
 
 fn main() {
-    println!("{}", grove_coordinates(input::USER));
+    println!("{}", part1(input::USER));
 }
 
 #[test]
 fn test_example() {
-    assert_eq!(3, grove_coordinates(input::EXAMPLE));
+    assert_eq!(3, part1(input::EXAMPLE));
 }
--- day20/src/lib.rs
+++ day20/src/lib.rs
@@ -1,1 +1,58 @@
 pub mod input;
+
+fn grove_coordinates(input: &str, decryption_key: isize, rounds: u8) -> isize {
+    let sequence: Vec<isize> = input
+        .lines()
+        .map(|line| line.parse::<isize>().unwrap() * decryption_key)
+        .collect();
+    let mut mixed: Vec<usize> = (0..sequence.len()).collect();
+
+    for _round in 0..rounds {
+        for (seq_index, number) in sequence.iter().enumerate() {
+            let index = mixed
+                .iter()
+                .enumerate()
+                .find(|(_, i)| i == &&seq_index)
+                .unwrap()
+                .0;
+            let next_index_isize = (index as isize + number) % (sequence.len() as isize - 1);
+            let next_index = if next_index_isize < 0 {
+                sequence.len() - 1 - (next_index_isize.unsigned_abs() % (sequence.len() - 1))
+            } else {
+                next_index_isize as usize
+            };
+
+            assert!(index < sequence.len());
+            assert!(next_index < sequence.len());
+            match next_index.cmp(&index) {
+                std::cmp::Ordering::Greater => mixed[index..=next_index].rotate_left(1),
+                std::cmp::Ordering::Less => mixed[next_index..=index].rotate_right(1),
+                std::cmp::Ordering::Equal => {}
+            }
+        }
+    }
+
+    let seq_index0 = sequence
+        .iter()
+        .enumerate()
+        .find(|(_, number)| number == &&0)
+        .unwrap()
+        .0;
+    let index0 = mixed
+        .iter()
+        .enumerate()
+        .find(|(_, index)| index == &&seq_index0)
+        .unwrap()
+        .0;
+    sequence[mixed[(index0 + 1000) % sequence.len()]]
+        + sequence[mixed[(index0 + 2000) % sequence.len()]]
+        + sequence[mixed[(index0 + 3000) % sequence.len()]]
+}
+
+pub fn part1(input: &str) -> isize {
+    grove_coordinates(input, 1, 1)
+}
+
+pub fn part2(input: &str) -> isize {
+    grove_coordinates(input, 811589153, 10)
+}
